8baab0ff74d6fa4bba2ff28dbd3c51203ec01c46
rv-work <rv.dthought@gmail.com>
2025-09-02 10:07:37 +0530

    CloudFlare

--- Backend/index.js
+++ Backend/index.js
@@ -21,6 +21,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173', 
     'http://0.0.0.0:5001',
+    'https://dr-av-instructors-threat.trycloudflare.com',
     '*'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
