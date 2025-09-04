e9f55adc1f8a05d7a6c2a30a85ee7b6c57b81554
rv.dthought@gmail.com <rv.dthought@gmail.com>
2025-09-04 23:42:24 +0530

    fix

--- Backend/Controllers/AuthControllers.js
+++ Backend/Controllers/AuthControllers.js
@@ -265,7 +265,7 @@ export const Login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', 
-      sameSite: 'Lax', 
+      sameSite: 'None', 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
