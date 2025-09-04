8538451f21ee141cef95ffe62fb8c677a0d11f55
rv.dthought@gmail.com <rv.dthought@gmail.com>
2025-09-05 01:57:27 +0530

    fix

--- Backend/Socket.js
+++ Backend/Socket.js
@@ -4,9 +4,9 @@ import {Server} from 'socket.io';
 const consultationStates = new Map();
 
 export const initializeSocket = (server) => {
-  const io = new Server(server, {
+    const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000", "https://medlink-frontend.vercel.app"],
+      origin: ["http://localhost:5173", "https://med-link-rvn.vercel.app"],
       methods: ["GET", "POST"],
       credentials: true
     }
